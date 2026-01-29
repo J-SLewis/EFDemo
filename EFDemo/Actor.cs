beff494481c09ce8fee3cffd89ad2f11426a1930
Kimberly Collins <kimberlyacollins@gmail.com>
2026-01-29 14:27:42 -0600

    Add DbContext and Actor class

---
+++ EFDemo/Actor.cs
@@ -0,0 +1,11 @@
+﻿namespace EFDemo
+{
+    public class Actor
+    {
+        public int ActorId { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public DateTime LastUpdate { get; set; }
+    }
+
+}
---
+++ EFDemo/DbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFDemo
+{
+    public class PagilaContext : DbContext
+    {
+        public DbSet<Actor> Actors => Set<Actor>();
+
+        public PagilaContext(DbContextOptions<PagilaContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor>(entity =>
+            {
+                entity.ToTable("actor");
+
+                entity.HasKey(e => e.ActorId);
+
+                entity.Property(e => e.ActorId)
+                      .HasColumnName("actor_id");
+
+                entity.Property(e => e.FirstName)
+                      .HasColumnName("first_name")
+                      .HasMaxLength(45)
+                      .IsRequired();
+
+                entity.Property(e => e.LastName)
+                      .HasColumnName("last_name")
+                      .HasMaxLength(45)
+                      .IsRequired();
+
+                entity.Property(e => e.LastUpdate)
+                      .HasColumnName("last_update");
+            });
+        }
+    }
+}
